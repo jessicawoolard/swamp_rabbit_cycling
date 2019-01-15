f68324e74a0482aa6a547cad35a59fc0b44ed867
Jessica Woolard <jessicawoolard@Jessicas-MacBook-Pro.local>
2019-01-14 23:26:51 -0500

    update on styles

--- app.py
+++ app.py
@@ -3,31 +3,36 @@ import csv
 from flask import Flask
 from flask import request
 import requests
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-   # Etsy: https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=bicycle&includes=Images,Shop&sort_on=score
+    # Etsy: https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=bicycle&includes=Images,Shop&sort_on=score
 
-   response = requests.get('https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=bicycle&includes=Images,Shop&sort_on=score')
-   data = response.json()
+    response = requests.get('https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=bicycle&includes=Images,Shop&sort_on=score')
+    data = response.text.replace("/**/", "").replace(");", "")
 
-   etsy_listings = data['results']
+    valid_json = json.loads(data)
+    print(valid_json)
 
-   photos = []
+    etsy_listings = valid_json['results']
 
-   for etsy_listing in etsy_listings:
-       # photos.append(
-        print(etsy_listing['Images')
+    photos = []
 
-   # planet_html = ''.join(planet_html)
+    for etsy_listing in etsy_listings:
+        # photos.append(
 
-   # Get html and render to screen
-   index_file = open('index.html', 'r')
-   index_html = index_file.read()
-   index_html = index_html.replace('{{planet_list}}', planet_html)
-   index_file.close()
-   #
-   return index_html
+        print(etsy_listing['Images'])
+
+    # planet_html = ''.join(planet_html)
+
+    # Get html and render to screen
+    index_file = open('index.html', 'r')
+    index_html = index_file.read()
+    # index_html = index_html.replace('{{planet_list}}', planet_html)
+    index_file.close()
+
+    return index_html
